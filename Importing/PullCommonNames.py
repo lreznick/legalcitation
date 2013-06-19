af6c4913de0709f97b9178ac713de24e94c53523
David Pardy <djspardy@gmail.com>
2013-06-18 18:28:14 -0600

    Played with file to get the common names from the wikipedia page
    It won't work with regular expressions, so I'll do it manually.

--- Importing/PullCommonNames.py
+++ Importing/PullCommonNames.py
@@ -12,14 +12,24 @@ Com = []
 Off = []
 
 
-common_one = re.compile("[\w\s]+(?=\]\] <br />\(common, English\))")
-
+common_one = re.compile("[\w\s]+(?=\]\]\s?<br />)")
+common_two = re.compile("[\w\s]+(?=\]\]''' \(common, English\))")
+common_three = re.compile("[\w\s]+(?=''' \(common, English\))")
+#common_four = re.compile("[\w\s]+(?=\]\]\s?<br />\(shortened form\))")
 
 for line in f:
-	match = common_one.search(line)
-	if match:
-		print line
-		print match.group()
+	match_one = common_one.search(line)
+	match_two = common_two.search(line)
+	match_three = common_three.search(line)
+	#match_four = common_four.search(line)
+	if match_one:
+		print match_one.group()
+	if match_two:
+		print match_two.group()
+	if match_three:
+		print match_three.group()
+	#if match_four:
+	#	print match_four.group()
 
 		
 
