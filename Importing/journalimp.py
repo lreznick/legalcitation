69f87039d15873561aa3fd15cb815137142e22ed
David Pardy <djspardy@gmail.com>
2013-08-04 18:05:49 -0600

    Almost done Journals

--- Importing/journalimp.py
+++ Importing/journalimp.py
@@ -4,6 +4,8 @@ import re
 import unicodedata
 
 
+
+
 def safe_str(obj):
     """ return the byte string representation of obj """
     try:
@@ -12,14 +14,9 @@ def safe_str(obj):
         # obj is unicode
         return unicode(obj).encode('unicode_escape')
         
-def chunks(l, n):
-    """ Yield successive n-sized chunks from l.
-    """
-    for i in xrange(0, len(l), n):
-        yield l[i:i+n]
+
 
 Journals = []
-
 
 
 wb = xlrd.open_workbook('journals.xlsx') # open in read mode
@@ -31,9 +28,9 @@ for rownum in range(sh.nrows):
     Row = sh.row_values(rownum)
     Row[0] = safe_str(Row[0].strip())
     Row[1] = safe_str(Row[1].strip())
-    print Row
+	#print Row
     Journals.append(Row)
 
-
+print Journals
 
 
