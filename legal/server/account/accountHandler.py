0c72131e5d2576ac4131937d028f4e5c13f84a9b
Stephen <stephenhuang1@gmail.com>
2013-08-29 21:45:06 -0600

    modified usability on account

--- legal/server/account/accountHandler.py
+++ legal/server/account/accountHandler.py
@@ -5,15 +5,27 @@ import globs
 
 urls = (
 	'' , 'Account'
-
-	
 )
 	
+class account:
+	def __init__(self, email, occupation, school):
+		self.email = email
+		self.occupation =occupation
+		self.school = school
+
+		
+class subscription:
+	def __init__(self,  subscript, exp):
+		self.subscript = subscript
+		self.exp = exp
+		
 class Account(object):
 	def GET(self):
 		print "hello"
 		##return web.template.frender('webclient/templates/account/accountMain.html')
-		return globs.render.accountMain()
+		a = account("qwert1_2@hotmail.com", "student", "U&#160;of&#160;Toronto", )
+		b = subscription("Free Year Subscription", "Sept. 1, 2014")
+		return globs.render.accountMain(a,b)
 	
 
 app_accountHandler = web.application(urls, locals())
