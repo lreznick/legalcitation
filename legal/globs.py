26a115e1d2a52969999545040220e15c7856754e
Gerras <k.brauen@gmail.com>
2013-08-30 17:57:25 -0600

    added changing passwords/emails, made some more shit global.

--- legal/globs.py
+++ legal/globs.py
@@ -1,17 +1,47 @@
 import web
-
+import random
+from passlib.context import CryptContext
 
 def init():
 
 	
 	global db
 	global render
+	global pwd_context
 	
-	db = web.database(dbn='mysql', host='127.0.0.1', port=3306, user='root', pw='root', db='intravires')
-	#db = web.database(dbn='mysql', host='127.0.0.1', port=3306, user='root', pw='Jeenyus1', db='intravires')
+#	db = web.database(dbn='mysql', host='127.0.0.1', port=3306, user='root', pw='root', db='intravires')
+	db = web.database(dbn='mysql', host='127.0.0.1', port=3306, user='root', pw='Jeenyus1', db='intravires')
 	
 	
 	template_globals ={ 'str': str }
 	render_partial = web.template.render('webclient/templates/', globals=template_globals)
 	render = web.template.render('webclient/templates/', globals=template_globals, base='layout')
 	template_globals.update(render=render_partial)
+	
+	
+	pwd_context = CryptContext(
+    # replace this list with the hash(es) you wish to support.
+    # this example sets pbkdf2_sha256 as the default,
+    # with support for legacy des_crypt hashes.
+    schemes=["sha512_crypt"],
+    default="sha512_crypt",
+	all__vary_rounds = 0.1,
+	sha512_crypt__default_rounds = 8000,
+	)
+
+def verify_user_hash(unverified_pwd, query_result):
+	#print "INSIDE VERIFY USER!!!!"
+	v_salt = query_result.salt
+	v_hash = query_result.hash
+	ok = pwd_context.verify(unverified_pwd + v_salt, v_hash)
+	#print ok
+	return ok
+
+class PasswordHash(object):
+	def __init__(self, password_):
+		self.salt = "".join(chr(random.randint(33,127)) for x in xrange(60))
+		self.hashedpw = pwd_context.encrypt(password_ + self.salt)
+		
+	def check_password(self, hash2):
+		"""checks if the password is correct"""
+		return pwd_context.verify(password_+self.salt, self.hashedpw)	
--- legal/server/subapplications/dbConnector.py
+++ legal/server/subapplications/dbConnector.py
@@ -11,6 +11,7 @@ import globs
 # create a single global instance for your app...
 #
 
+'''
 pwd_context = CryptContext(
     # replace this list with the hash(es) you wish to support.
     # this example sets pbkdf2_sha256 as the default,
@@ -19,7 +20,8 @@ pwd_context = CryptContext(
     default="sha512_crypt",
 	all__vary_rounds = 0.1,
 	sha512_crypt__default_rounds = 8000,
-	) 
+	)
+''' 
 #db = web.database(dbn='mysql', host='127.0.0.1', port=3306, user='root', pw='root', db='intravires')
 
 urls = (
@@ -42,36 +44,31 @@ signin_form = form.Form(form.Textbox('username',
                         form.Password('password',description='Password:'),
                         validators = [form.Validator("Username and password didn't match.",
                                       lambda x: users[x.username].check_password(x.password)) ])			
-								  
+'''								  
 class PasswordHash(object):
 	def __init__(self, password_):
 		self.salt = "".join(chr(random.randint(33,127)) for x in xrange(60))
-		self.hashedpw = pwd_context.encrypt(password_ + self.salt)
+		self.hashedpw = globs.pwd_context.encrypt(password_ + self.salt)
 
 	def check_password(self, hash2):
 		"""checks if the password is correct"""
-		return pwd_context.verify(password_+self.salt, self.hashedpw)	
-										  
-users = {
-    'Kermit' : PasswordHash('frog'), 
-    'ET' : PasswordHash('eetee'),  
-    'falken' : PasswordHash('joshua') }									  
-
+		return globs.pwd_context.verify(password_+self.salt, self.hashedpw)								  
+'''
 
 
 
 
 #for data in results: #members is IterBetter
 					#print data.user_id #member is Storage	
-
+'''
 def verify_user_hash(unverified_pwd, query_result):
 	#print "INSIDE VERIFY USER!!!!"
 	v_salt = query_result.salt
 	v_hash = query_result.hash
-	ok = pwd_context.verify(unverified_pwd + v_salt, v_hash)
+	ok = globs.pwd_context.verify(unverified_pwd + v_salt, v_hash)
 	#print ok
 	return ok
-					
+'''					
 					
 def handle_user(user_email, password, function_type):
 	try:
@@ -80,7 +77,7 @@ def handle_user(user_email, password, function_type):
 		print check_username
 		if (function_type == "register"):
 			if not list(check_username):
-				hashobj = PasswordHash(password)
+				hashobj = globs.PasswordHash(password)
 				user_ident = globs.db.query ("SELECT MAX(user_id) as highestId from users")[0]
 
 				if (user_ident.highestId == None):
@@ -94,7 +91,7 @@ def handle_user(user_email, password, function_type):
 				return False
 		elif (function_type == "login"):
 			results = check_username[0]
-			verified = verify_user_hash(password, results)
+			verified = globs.verify_user_hash(password, results)
 			return verified
 
 			
