1f326b55cd45c744fa1954c7cc1900e6036f93a0
Gerras <k.brauen@gmail.com>
2013-08-30 15:02:31 -0600

    added retrieve users for user accounts page.

--- legal/server/account/accountHandler.py
+++ legal/server/account/accountHandler.py
@@ -7,6 +7,22 @@ urls = (
 	'' , 'Account'
 )
 	
+	
+	
+'''
+Table: users
+Columns:
+user_id			int(11) PK
+email			varchar(50)
+create_date		datetime
+active			tinyint(1)
+salt			char(64)
+hash			blob
+occupation		varchar(45)
+school			varchar(45)
+'''
+	
+	
 class account:
 	def __init__(self, email, occupation, school):
 		self.email = email
@@ -21,11 +37,15 @@ class subscription:
 		
 class Account(object):
 	def GET(self):
-		print "hello"
+		#print "hello"
 		##return web.template.frender('webclient/templates/account/accountMain.html')
-		a = account("qwert1_2@hotmail.com", "student", "U&#160;of&#160;Toronto", )
+		
+		user_name = web.ctx.session.username
+		userQuery = globs.db.query("SELECT * FROM users WHERE email=$user", vars={'user':user_name})[0]
+		
+		#a = account("qwert1_2@hotmail.com", "student", "U&#160;of&#160;Toronto", )
 		b = subscription("Free Year Subscription", "Sept. 1, 2014")
-		return globs.render.accountMain(a,b)
+		return globs.render.accountMain(userQuery,b)
 	
 
 app_accountHandler = web.application(urls, locals())
